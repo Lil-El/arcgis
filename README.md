a7ec81f417299daa2162953fc5deb69601d1404e
unknown <yxd99324@qq.com>
2020-09-29 16:30:00 +0800

    arcgis base map load

--- README.md
+++ README.md
@@ -1,24 +1,10 @@
-# arcgis
+# ArcGIS 学习-基于 Vue3.0
 
-## Project setup
+## Project
+
 ```
 npm install
-```
-
-### Compiles and hot-reloads for development
-```
 npm run serve
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+See [Vue Composition API](https://composition-api.vuejs.org/zh/)
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <div>Count: {{ state.count }}</div>
+      <div>Double: {{ state.double }}</div>
+      <button @click="increment">COUNT++</button>
+    </div>
+    <div id="map"></div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { loadModules } from "esri-loader";
+import { computed, onMounted, reactive } from "vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  setup() {
+    const state = reactive({
+      count: 0,
+      double: computed(() => state.count * 2),
+      age: "arcgis",
+    });
+    function increment() {
+      state.count++;
+    }
+    onMounted(handleMounted);
+    return {
+      state,
+      increment,
+    };
+  },
+};
+function handleMounted() {
+  loadModules(["esri/Map", "esri/views/MapView"]).then(([Map, MapView]) => {
+    let map = new Map({
+      basemap: "hybrid", // streets，hybrid
+    });
+
+    let view = new MapView({
+      container: "map",
+      map,
+      center: [106, 34.09042],
+      zoom: 3,
+    });
+  });
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+#map {
+  width: 100%;
+  height: 500px;
 }
 </style>
