3e70cfdb48f0a9bef78bb49ac0693ab801cd9de4
unknown <yxd99324@qq.com>
2020-11-18 17:31:35 +0800

    useStore

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -2,9 +2,12 @@ import { reactive } from "vue";
 
 const store = { map: null, view: null };
 
-export default function useStore() {
+export default function useStore({ map, view }) {
   // if (store.map) {
-
+  if (map || view) {
+    store.map = map;
+    store.view = view;
+  }
   return store;
   // } else {
   //   const state = reactive({
