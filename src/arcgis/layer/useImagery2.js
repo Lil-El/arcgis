5afb088fdcd148e81d4537a2010a9732cf22396f
yanxd <yxd99324@qq.com>
2024-02-19 10:44:19 +0800

    arcgis server test.

--- src/arcgis/layer/useImagery2.js
+++ src/arcgis/layer/useImagery2.js
@@ -1,4 +1,5 @@
 import ImageryLayer from "@arcgis/core/layers/ImageryLayer";
+import ImageryTileLayer from "@arcgis/core/layers/ImageryTileLayer";
 import * as rasterFunctionUtils from "@arcgis/core/layers/support/rasterFunctionUtils";
 import RasterStretchRenderer from "@arcgis/core/renderers/RasterStretchRenderer";
 import RasterFunction from "@arcgis/core/layers/support/RasterFunction";
@@ -68,6 +69,15 @@ export default () => {
   let definitionExpression1 = `dcpc like '2022年12月12日至2022年12月12日%' AND ksbm = 'C1000002008091120000816'`;
   // 曹家滩DEM
   let definitionExpression2 = `dcpc like '2023年7月18日至2023年8月29日%' AND ksbm = 'C6100002017111110145309'`;
+
+  // online
+  // DEMUrl = "https://sampleserver6.arcgisonline.com/arcgis/rest/services/OsoLandslide/OsoLandslide_After_3DTerrain/ImageServer";
+
+  // test
+  // DEMUrl = "https://cportal.beidouhj.com/server/rest/services/image_test/C6100002017111110145309_DEM_CaoJiaTan2023/ImageServer";
+  // DEMUrl = "https://cportal.beidouhj.com/server/rest/services/image_test/test_db_1_shixiuan_CaoJiaTan_DEM/ImageServer"
+  // DEMUrl = "https://cportal.beidouhj.com/server/rest/services/Hosted/C6100002017111110145309_CaoJiaTan_DEM_2023_001/ImageServer"
+  DEMUrl = "https://cportal.beidouhj.com/server/rest/services/image_test/CaoJiaTan2023_DEM_003/ImageServer";
   //#endregion
 
   if (map.findLayerById("IMAGERY_LAYER")) {
@@ -76,35 +86,41 @@ export default () => {
     const layer = new ImageryLayer({
       id: "IMAGERY_LAYER",
       url: DEMUrl,
-      definitionExpression: definitionExpression2,
+      // definitionExpression: definitionExpression2,
     });
 
     layer.when(() => {
+      // set 1
       const stretchFunction = new RasterFunction({
         rasterFunction: "Stretch",
         rasterFunctionArguments: {
           StretchType: 5,
           Min: 0,
           Max: 255,
+          Raster: "$$",
         },
         outputPixelType: "U8",
         variableName: "Raster",
       });
 
-      const colorFunction = new RasterFunction({
+      let colorFunction = new RasterFunction({
         functionName: "Colormap",
         functionArguments: {
           ColorrampName: "Elevation #1",
           // Raster: stretchFunction,
         },
         outputPixelType: "U8",
+        variableName: "Raster",
       });
-      // console.log(rasterFunctionUtils);
-      // const colormap = rasterFunctionUtils.colormap({
-      //   colorRampName: "Elevation #1", // Blue Bright
-      // });
 
-      layer.rasterFunction = colorFunction;
+      // set 2
+      const colormap = rasterFunctionUtils.colormap({
+        colorRampName: "Elevation #1", // Blue Bright
+        // outputPixelType: "U8",
+        // raster: stretchFunction,
+      });
+
+      layer.rasterFunction = colormap;
 
       view.goTo(layer.fullExtent);
     });
