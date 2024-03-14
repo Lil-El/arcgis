aa2a49a15a955ef4cdc6440ca951ca6dcbe9a6e4
yanxd <yxd99324@qq.com>
2024-03-14 09:26:25 +0800

    client render feature graphics.

--- src/arcgis/layer/useFeature.js
+++ src/arcgis/layer/useFeature.js
@@ -1,6 +1,7 @@
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 import Graphic from "@arcgis/core/Graphic";
 import useMap from "../map/useMap";
+import LabelClass from "@arcgis/core/layers/support/LabelClass";
 
 /**
  * Tips:
@@ -13,8 +14,19 @@ export default () => {
   if (map.findLayerById("FEATURE_LAYER")) {
     map.remove(map.findLayerById("FEATURE_LAYER"));
   } else {
+    const statesLabelClass = new LabelClass({
+      labelExpressionInfo: { expression: "$feature.name" },
+      symbol: {
+        type: "text",
+        color: "red",
+        haloSize: 1,
+        haloColor: "white",
+      },
+    });
+
     const layer = new FeatureLayer({
       id: "FEATURE_LAYER",
+      labelingInfo: [statesLabelClass],
       objectIdField: "ObjectID",
       outFields: ["*"], // 查询的字段
       // 配置的字段，和 graphic 的 attributes 相关联
@@ -30,9 +42,9 @@ export default () => {
             longitude: 108.90039062499926,
             latitude: 34.27219171167824,
           },
-          symbol: {
+          symbol: { // 不生效
             type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
-            color: [226, 119, 40],
+            color: [0, 255, 0],
           },
           attributes: {
             name: "Mino",
@@ -40,6 +52,15 @@ export default () => {
           },
         }),
       ],
+      // However, when creating a FeatureLayer from client-side features,
+      // this property must be specified in the layer's constructor along with the source, fields, objectIdField properties.
+      renderer: {
+        type: "simple",
+        symbol: {
+          type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+          color: [255, 0, 0],
+        },
+      },
       popupTemplate: {
         title: "This's Title!",
         outFields: ["*"],
